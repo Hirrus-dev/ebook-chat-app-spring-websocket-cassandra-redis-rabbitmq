4820443b4ceacac8c18b8136a7a6d6315f7009a7
Hirrus-dev <68560773+Hirrus-dev@users.noreply.github.com>
2020-08-26 15:33:07 +0300

    add generate war

--- ebook-chat/src/main/java/br/com/jorgeacetozi/ebookChat/Application.java
+++ ebook-chat/src/main/java/br/com/jorgeacetozi/ebookChat/Application.java
@@ -2,11 +2,18 @@ package br.com.jorgeacetozi.ebookChat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+                return application.sources(Application.class);
+        }
+
+        public static void main(String[] args) {
+                SpringApplication.run(Application.class, args);
+        }
 }
